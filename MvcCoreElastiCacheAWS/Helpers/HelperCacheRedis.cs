9eb04d0c25f2c62dfb9c4a74eca25876365c52a3
Jaimee22 <alumno.410564@ies-azarquiel.es>
2024-05-21 13:25:03 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect("");
+        });
+
+
+        public ConnectionMultiplexer Connection 
+        {
+            get 
+            {
+                return CreateConnection.Value;
+            }
+        }
+
+
+
+    }
+}
